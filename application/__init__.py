77c2786ecc9102d5e8f3fa2cb725d5c2ea34eef5
gu9 <gss.gurpreetsaini@gmail.com>
2021-01-02 06:25:06 -0800

    added maail configs settings

--- application/__init__.py
+++ application/__init__.py
@@ -1,4 +1,12 @@
 from flask import Flask
+from flask_mail import Mail
+import os
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = os.environ.get("SECRETKEY")
+app.config["MAIL_SERVER"] = "smtp.hushmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = os.environ.get("USER_MAIL")
+app.config["MAIL_PASSWORD"] = os.environ.get("PASSWORD")
+mail = Mail(app)
 from application import routes
